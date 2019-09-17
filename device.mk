89a2f1f7e8679deeb82179195c971e5c7886d6a3
zahidmgsk <zahidmgsk@gmail.com>
2019-09-17 06:03:24 +0000

    Liquid Remix 10.0.11-ify

--- device.mk
+++ device.mk
@@ -331,10 +331,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
-# LineageActions
+# CMActions
 PRODUCT_PACKAGES += \
-    LineageActions \
-    libjni_LineageActions
+    CMActions \
+    libjni_CMActions
 
 # Keymaster HIDL interfaces
 PRODUCT_PACKAGES += \
---
+++ liquid.mk
@@ -0,0 +1,8 @@
+# Inherit device configuration
+$(call inherit-product, device/motorola/victara/liquid_victara.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := liquid_victara
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := Moto x 2014
+TARGET_MANUFACTURER := motorola
