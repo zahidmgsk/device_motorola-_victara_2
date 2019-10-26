a3d44a98d5b6477dd4709019272795deef1257dc
bohu <bohu@google.com>
2019-10-26 12:56:00 +0100

    victara: Disable codec2
    BUG: 133178866
    Change-Id: Ie6698604357adfb7c36d6655ea8a42aecad6b219

--- system_prop.mk
+++ system_prop.mk
@@ -49,6 +49,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # MEDIA
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.ccodec=0 \
     debug.stagefright.omx_default_rank.sw-audio=1 \
     debug.stagefright.omx_default_rank=0 \
     persist.media.treble_omx=false
