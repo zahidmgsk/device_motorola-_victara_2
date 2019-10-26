2e3f5a8188607d9cfebeff058e47c8bdab4cd5be
Sujin Panicker <spanic@codeaurora.org>
2019-10-26 13:00:02 +0100

    victara: fix rank of OMX audio codec components
    Rank OMX SW audio codecs higher than C2 codecs.
    *Also alfabetize the makefile

--- system_prop.mk
+++ system_prop.mk
@@ -21,10 +21,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.audio.calfile5=/vendor/etc/acdbdata/Headset_cal.acdb \
     persist.audio.calfile6=/vendor/etc/acdbdata/Speaker_cal.acdb
 
-# FM maximum volume be between 0 and 8192 (0dB)
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.audio.fm_max_volume=5793
-
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     qcom.bluetooth.soc=smd
@@ -33,11 +29,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1
 
-# GPS
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.mot.gps.smart_battery=1
-
-# Graphics
+# Display
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true \
     ro.hdmi.enable=true \
@@ -45,8 +37,41 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qualcomm.cabl=0 \
     ro.sf.lcd_density=480 \
     debug.hwui.use_buffer_age=false \
-	debug.sf.enable_gl_backpressure=1 \
+    debug.sf.enable_gl_backpressure=1
+
+# FM maximum volume be between 0 and 8192 (0dB)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.audio.fm_max_volume=5793
+
+# GPS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.mot.gps.smart_battery=1
+
+# MEDIA
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0 \
     persist.media.treble_omx=false
+
+# NITZ
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.rild.nitz_plmn="" \
+    persist.rild.nitz_long_ons_0="" \
+    persist.rild.nitz_long_ons_1="" \
+    persist.rild.nitz_long_ons_2="" \
+    persist.rild.nitz_long_ons_3="" \
+    persist.rild.nitz_short_ons_0="" \
+    persist.rild.nitz_short_ons_1="" \
+    persist.rild.nitz_short_ons_2="" \
+    persist.rild.nitz_short_ons_3=""
+
+# Other
+# Motorola's init sets this, let's just do it ourselves
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hw.device=victara
+# Play store
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase.gmm=android-motorola
 
 # QCOM
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -72,18 +97,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.use_data_netmgrd=true \
     DEVICE_PROVISIONED=1
 
-# NITZ
-PRODUCT_PROPERTY_OVERRIDES += \
-	persist.rild.nitz_plmn="" \
-	persist.rild.nitz_long_ons_0="" \
-	persist.rild.nitz_long_ons_1="" \
-	persist.rild.nitz_long_ons_2="" \
-	persist.rild.nitz_long_ons_3="" \
-	persist.rild.nitz_short_ons_0="" \
-	persist.rild.nitz_short_ons_1="" \
-	persist.rild.nitz_short_ons_2="" \
-	persist.rild.nitz_short_ons_3=""
-	
 # Time services
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
@@ -92,9 +105,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.disableWifiApFirmwareReload=true
 
-# Other
-# Motorola's init sets this, let's just do it ourselves
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hw.device=victara
-# Play store
-    ro.com.google.clientidbase.gmm=android-motorola
