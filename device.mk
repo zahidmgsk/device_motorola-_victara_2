75534b9ab0f387c574514fe45443b6ea8cbcea93
jro1979oliver <jroliveira.oliveira301@gmail.com>
2020-02-07 14:31:20 +0000

    victara: add android.hardware.camera.provider@2.4-service

--- device.mk
+++ device.mk
@@ -146,6 +146,7 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     Snap \
     libshim_camera \
