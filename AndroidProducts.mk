dafd052db4ab266bfe42be2c7ad287a01cd76f70
jro1979oliver <jroliveira.oliveira301@gmail.com>
2020-02-16 17:46:21 +0000

    victara: Aicipify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,4 @@
+
 #
 # Copyright (C) 2014 The CyanogenMod Project
 #
@@ -14,8 +15,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_victara.mk
+    $(LOCAL_DIR)/aicp_victara.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_victara-eng \
-    lineage_victara-userdebug
+    aicp_victara-eng \
+    aicp_victara-userdebug
--- aicp_victara.mk
+++ aicp_victara.mk
@@ -1,15 +1,15 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common aicp stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Inherit from victara device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := victara
-PRODUCT_NAME := lineage_victara
+PRODUCT_NAME := aicp_victara
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := victara
 PRODUCT_RELEASE_NAME := MOTO X (2014)
