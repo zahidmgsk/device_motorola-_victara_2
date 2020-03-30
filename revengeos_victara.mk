1785585a450cd1e7f499ef252bc7e75e458add14
zahidmgsk <zahidmgsk@gmail.com>
2020-03-30 08:53:01 +0000

    RevengeOS-ify

--- revengeos_victara.mk
+++ revengeos_victara.mk
@@ -2,14 +2,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common aosp stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/revengeos/config/common.mk)
 
 # Inherit from victara device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := victara
-PRODUCT_NAME := aosp_victara
+PRODUCT_NAME := revengeos_victara
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := victara
 PRODUCT_RELEASE_NAME := MOTO X (2014)
@@ -24,9 +24,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
 # Use Gapps
-WITH_GAPPS := false
-TARGET_GAPPS_ARCH := arm
-IS_PHONE := true
+#WITH_GAPPS := false
+#TARGET_GAPPS_ARCH := arm
+#IS_PHONE := true
 
 # AospExtended-BuildType
-EXTENDED_BUILD_TYPE := OFFICIAL
+#EXTENDED_BUILD_TYPE := OFFICIAL
